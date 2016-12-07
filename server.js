082630ff8c9f50a570ae7ef30e2c6cf3324adae3
Che B <cnakamura336@gmail.com>
2016-12-07 14:48:20 -0500

    update token

--- server.js
+++ server.js
@@ -1,44 +1,36 @@
 const express = require('express');
-const fs = require('fs');
-const bodyParser = require('body-parser');
-const marked = require('marked');
-const token = 'Keep me a secret';
-const uuid = require('uuid');
 const app = express();
+const randomString = require('randomstring');
+const uuid = require('node-uuid');
+// auth token - you would normally generate a random one
+const token = 'Keep me a secret';
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
 
-
-
-
-
-// app.use(express.static('public'));
 function auth(req, res, next) {
-  app.use(bodyParser.json());
-  app.set('view engin', 'hbs');
-
-    if(req.query.token === token){
-      next();
-    }
-    else{
-      res.status(401);
-      res.json({ error: 'you arn t logged in.'});
-    }
+  // verify the authentication token
+  if (req.query.token === token) {
+    next();
+  } else {
+    res.status(401);
+    res.json({ error: 'You are not logged in lol' });
   }
 }
 
-app.post('/api/login', function(req, res){
+app.post('/api/login', function(req, res) {
   var username = req.body.username;
   var password = req.body.password;
-
-  if(usename === 'larry' && password === 'open'){
-
-
+  // verify the login
+  if (username === 'larry' && password === 'open') {
+    // return the auth token, normally you would
+    // generate a random one here, but we are
+    // using a hardcoded token, see above
     res.json({
       username: username,
-      token: token
+      token: uuid.v4()
     });
-  }
-  else{
+  } else {
     res.status(401);
     res.json({
       error: 'Login incorrect'
@@ -46,6 +38,16 @@ app.post('/api/login', function(req, res){
   }
 });
 
-app.listen(3000, function(){
-  console.log("Example app listening on part 3000");
+// make use of the auth middleware to validate
+// that the user has been logged in
+app.get('/api/data', auth, function(req, res) {
+      var theData = [
+        { name: 'blah' },
+        { name: 'more blah' },
+      ];
+      res.json(theData);
+    });
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
 });
