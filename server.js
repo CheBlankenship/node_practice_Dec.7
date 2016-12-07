f1dd02cc1274192b48ee43a8a22caf658fa7b928
Che B <cnakamura336@gmail.com>
2016-12-07 13:38:26 -0500

    update3

--- server.js
+++ server.js
@@ -2,34 +2,49 @@ const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const marked = require('marked');
-
+const token = 'Keep me a secret';
+const uuid = require('uuid');
 const app = express();
-app.use(express.static('public'));
-app.use(bodyParser.json());
-// app.set('view engin', 'hbs');
 
-function auth(req, res, next){
-  if(req.query.token === token){
-    next();
-  }
-  else{
-    res.status(401);
-    res.json({ error: 'You r not login'});
+
+
+
+
+
+// app.use(express.static('public'));
+function auth(req, res, next) {
+  app.use(bodyParser.json());
+  app.set('view engin', 'hbs');
+
+    if(req.query.token === token){
+      next();
+    }
+    else{
+      res.status(401);
+      res.json({ error: 'you arn t logged in.'});
+    }
   }
 }
 
-app.use(function myMiddleware(request, response, next){
-  console.log(request.method, request.path);
-  next();
-});
+app.post('/api/login', function(req, res){
+  var username = req.body.username;
+  var password = req.body.password;
 
-app.get('/', function I_am_middleware(req, res, next){
-  res.send('Hello world!');
-}, function(req,res, next) {
-  console.log('Check ifs working');
-  next();
-});
+  if(usename === 'larry' && password === 'open'){
 
+
+    res.json({
+      username: username,
+      token: token
+    });
+  }
+  else{
+    res.status(401);
+    res.json({
+      error: 'Login incorrect'
+    });
+  }
+});
 
 app.listen(3000, function(){
   console.log("Example app listening on part 3000");
